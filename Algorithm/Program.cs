f4bc8c4ceac8d4d6f10d4311c428a3609a68f555
Alexandr Sivohin <alexandrsivoh@gmail.com>
2021-04-05 18:53:38 +0300

    Добавьте файлы проекта.

---
+++ Algorithm/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Algorithm
+{
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Get array and sets user
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            User user1 = new User(
+                "Vasy",
+                "Василий",
+                true
+                );
+            User user2 = new User(
+                "bijg12354",
+                "Александр",
+                false
+                );
+            User user3 = new User(
+                "alsd245",
+                "Евгений",
+                true
+                );
+            User user4 = new User(
+                "Princ",
+                "Рой",
+                true
+                );
+            User[] userArr = new User[] { user1, user2, user3, user4 };
+
+            for (int i = 0; i < userArr.Length; i++)
+            {
+                Console.WriteLine("Здравствуйте {0}", userArr[i].Name);
+                if (!userArr[i].IsPremium)
+                {
+                    ShowAds();
+                }
+            }
+            Console.WriteLine("для остановки нажмите Enter");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Method show ads.
+        /// </summary>
+        private static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            //// Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            //// Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            //// Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+    }
+}
---
+++ Algorithm/User.cs
@@ -0,0 +1,44 @@
+﻿namespace Algorithm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Dada user.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User" /> class.
+        /// Join properties to parameters.
+        /// </summary>
+        /// <param name="login">This login.</param>
+        /// <param name="name">This is name.</param>
+        /// <param name="isPremium">Check premium.</param>
+        public User(string login, string name, bool isPremium)
+        {
+            this.Login = login;
+            this.Name = name;
+            this.IsPremium = isPremium;
+        }
+
+        /// <summary>
+        /// Gets or sets a value the item is login.
+        /// </summary>
+        /// <value>The login of the customer.</value>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value the item is name.
+        /// </summary>
+        /// <value>The Name of the customer.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether  the item is IsPremium.
+        /// </summary>
+        /// <value> Does it have IsPremium.</value>
+        public bool IsPremium { get; set; }
+    }
+}
